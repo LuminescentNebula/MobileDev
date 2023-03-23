0bf7cfd0b03e7523b78f2ca32b5acdf605a55999
Dmitry <dima.lippo@bk.ru>
2023-03-23 11:32:47 +0300

    Practice 1 and 2

--- app/src/main/java/com/mirea/mobiledev/MainActivity.java
+++ app/src/main/java/com/mirea/mobiledev/MainActivity.java
@@ -2,18 +2,48 @@ package com.mirea.mobiledev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onButtonClick(View view)
+    {
+        Log.i(TAG, "Clicked!");
+    }
+
 
     private final String TAG = "onTag";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constraint_layout);
         Log.i(TAG,"onCreate");
+
+        TextView v= findViewById(R.id.textView2);
+        v.setText("Установленный текст");
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.raven);
+        Button button = findViewById(R.id.button2);
+        button.setText(R.string.nbtn);
+
+        View.OnClickListener onClickListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Clicked!");
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra(SecondActivity.TEXT, "Передалось!");
+                startActivity(intent);
+            }
+        };
+        button.setOnClickListener(onClickListener);
+
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
 
