eae26c5cadd04f44c09812a183e13cf88dea2ad3
Dmitry <dima.lippo@bk.ru>
2023-04-06 10:54:59 +0300

    optimized imports

--- app/src/main/java/com/mirea/mobiledev/MainActivity.java
+++ app/src/main/java/com/mirea/mobiledev/MainActivity.java
@@ -1,21 +1,9 @@
 package com.mirea.mobiledev;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentResultListener;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity {
 
