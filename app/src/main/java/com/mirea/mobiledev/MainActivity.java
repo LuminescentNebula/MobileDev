49dbbc7376be575aa24f46be6ef73a732b505909
LuminescentNebula <dima.lippo@bk.ru>
2023-05-01 15:10:48 +0300

    Practice 8

--- app/src/main/java/com/mirea/mobiledev/MainActivity.java
+++ app/src/main/java/com/mirea/mobiledev/MainActivity.java
@@ -1,15 +1,22 @@
 package com.mirea.mobiledev;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity {
 
     private final String TAG = "onTag";
+    private static final int code = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,34 +31,11 @@ public class MainActivity extends FragmentActivity {
                     .commit();
         }
 
-        getSupportFragmentManager().setFragmentResultListener("to", this, (requestKey, bundle) -> {
-            if (bundle.getBoolean("switch")){
-                getSupportFragmentManager().beginTransaction()
-                        .setReorderingAllowed(true)
-                        .replace(R.id.fragment_container_view,
-                                RecyclerFragment.class, bundle)
-                        .commit();
-            } else {
-                getSupportFragmentManager().beginTransaction()
-                        .setReorderingAllowed(true)
-                        .replace(R.id.fragment_container_view,
-                                ListFragment.class, bundle)
-                        .commit();
-            }
-        });
-    }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    "android.permission.READ_EXTERNAL_STORAGE",
+                    "android.permission.WRITE_EXTERNAL_STORAGE"}, code);
 
-    @Override
-    public void onBackPressed() {
-        if (getSupportFragmentManager().getFragments().get(0).getId()==R.layout.menu){
-            super.onBackPressed();
-        }
-        else {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .replace(R.id.fragment_container_view,
-                            MenuFragment.class, null)
-                    .commit();
         }
     }
 }
--- app/src/main/java/com/mirea/mobiledev/MenuFragment.java
+++ app/src/main/java/com/mirea/mobiledev/MenuFragment.java
@@ -1,109 +1,170 @@
 package com.mirea.mobiledev;
 
+
 import android.Manifest;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MenuFragment extends Fragment {
     View view;
+    private static final int code=0;
 
-    public MenuFragment(){
+    private static String TAG = "TAG";
+
+    private final   ActivityResultLauncher<String> resultContracts =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                Log.i(TAG,"registerForActivityResult");
+        if (isGranted) {
+            Log.i(TAG,"isGranted");
+            EditText editText = view.findViewById(R.id.field);
+            TextView textView = view.findViewById(R.id.text_line);
+            writeFileCommon(editText.getText().toString());
+            textView.setText(readFileCommon());
+        } else {
+            Log.i(TAG,"NotGranted");
+        }
+    });
+
+    public MenuFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.menu, container, false);
-        Bundle bundle = new Bundle();
 
-        view.findViewById(R.id.list).setOnClickListener(v -> {
-            if (bundle != null) {
-                bundle.putBoolean("switch", false);
-                getParentFragmentManager().setFragmentResult("to", bundle);
-            }
+
+        view.findViewById(R.id.app_specific).setOnClickListener(v -> {
+            EditText editText = view.findViewById(R.id.field);
+            writeFile(editText.getText().toString());
+            TextView textView = view.findViewById(R.id.text_line);
+            textView.setText(readFileSpecific());
         });
 
-        view.findViewById(R.id.recycler).setOnClickListener(v -> {
-            if (bundle != null) {
-                bundle.putBoolean("switch", true);
-                getParentFragmentManager().setFragmentResult("to", bundle);
-            }
-        });
+        view.findViewById(R.id.common).setOnClickListener(v -> {
+            Log.i(TAG,"Clicked");
 
-        view.findViewById(R.id.notification).setOnClickListener(v -> {
-            //Request POST NOTIFICATION permission, if not granted - request it again and show notification
-            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-                showNotification();
+            EditText editText = view.findViewById(R.id.field);
+            TextView textView = view.findViewById(R.id.text_line);
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.i(TAG,"Already");
+                writeFileCommon(editText.getText().toString());
+                textView.setText(readFileCommon());
             } else {
-                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-                    showNotification();
+                Log.i(TAG, "Before request");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                    requestPermissions(new String[]{
+//                            "android.permission.READ_EXTERNAL_STORAGE",
+//                            "android.permission.WRITE_EXTERNAL_STORAGE"}, code);
+
+//                ActivityCompat.requestPermissions(this,new String[]{
+//                        "android.permission.READ_EXTERNAL_STORAGE",
+//                        "android.permission.WRITE_EXTERNAL_STORAGE"},code);
+
+//                resultContracts.launch("android.permission.READ_EXTERNAL_STORAGE");
+               resultContracts.launch("android.permission.WRITE_EXTERNAL_STORAGE");
+
+                    writeFileCommon(editText.getText().toString());
+                    textView.setText(readFileCommon());
+
+                    Log.i(TAG, "After request");
                 }
             }
         });
 
-        view.findViewById(R.id.service).setOnClickListener(v -> {
-            //start NameService
-            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SYSTEM_ALERT_WINDOW) == PackageManager.PERMISSION_GRANTED) {
-                Log.i("OSS", "Is true");
-                Bundle bundle1 = new Bundle();
-                bundle1.putBoolean("service", true);
-                getParentFragmentManager().setFragmentResult("service", bundle1);
-            } else {
-                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW}, 1);
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SYSTEM_ALERT_WINDOW) == PackageManager.PERMISSION_GRANTED) {
-                    Log.i("OSS", "Is true");
-                    Bundle bundle1 = new Bundle();
-                    bundle1.putBoolean("service", true);
-                    getParentFragmentManager().setFragmentResult("service", bundle1);
-                }
-            }
-            Intent intent = new Intent(getActivity().getApplicationContext(), NameService.class);
-            getActivity().startService(intent);
+        view.findViewById(R.id.shared).setOnClickListener(v -> {
+
+        });
+
+        view.findViewById(R.id.room).setOnClickListener(v -> {
 
         });
         return view;
-    };
-
-
-    private void showNotification() {
-        NotificationManager notificationManager = (NotificationManager) getContext().getSystemService(getContext().NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("THE_ID", "CHANNEL_NAME",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("CHANNEL_DESC");
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        Notification.Builder builder = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            builder = new Notification.Builder(getContext(), "THE_ID")
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setContentTitle("My Notification")
-                    .setContentText("message")
-                    .setAutoCancel(true);
-        }
-
-        notificationManager.notify(0, builder.build());
     }
 
+    ;
+
+    void writeFile(String text) {
+        try {
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(getContext().openFileOutput("myfile", Context.MODE_PRIVATE)));
+            bw.write(text);
+            bw.close();
+            Log.d("FILE", "Файл записан");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String readFileSpecific() {
+        StringBuilder output = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(getContext().openFileInput("myfile")));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                output.append(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    void writeFileCommon(String text) {
+        File file = new File(Environment.getExternalStorageDirectory(), "example");
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            osw.write(text);
+            osw.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    String readFileCommon() {
+        File file = new File(Environment.getExternalStorageDirectory(), "example");
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            // You'll need to add proper error handling here
+        }
+        return text.toString();
+    }
 }
